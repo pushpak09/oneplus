3f01744a457806c89f87ac97460e1f2cc10146ae
Pushpak Chavan <pushpakchavan09@gmail.com>
2024-06-26 13:44:02 +0530

    Basic custom hook

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const LOGO_URL =
   "https://w7.pngwing.com/pngs/894/279/png-transparent-online-food-ordering-food-delivery-grubhub-others-food-service-logo.png";
+
+export const REST_MENU =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=";
